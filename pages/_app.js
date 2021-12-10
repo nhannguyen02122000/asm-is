5ebe6af186f5b58aecb14d0fbd1f6490db2487b3
Nhan Nguyen Thanh <nhannguyenthanh@shopee.com>
2021-12-10 09:12:57 +0700

    Add doc and modify app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import 'tailwindcss/tailwind.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+
+  return (
+    <>
+      <Head>
+        <title>AnimeN</title>
+        <meta name="description" content="Anime watching platform" />
+        <meta charSet="utf-8" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/iamdustan-smoothscroll/0.4.0/smoothscroll.min.js"></script>
+      </Head>
+      {/* <Provider store={store}> */}
+      <Component {...pageProps} />
+      {/* </Provider> */}
+      <WBLoader />
+      <GALoader />
+    </>
+  )
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './domains/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    // './dialogs/**/*.{js,ts,jsx,tsx}',
+    // './src/icons/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
